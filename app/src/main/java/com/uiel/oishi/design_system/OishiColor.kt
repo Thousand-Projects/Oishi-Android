c889a05d4133fac63d2586632c15834498425ca3
uiel <parkuiery@gmail.com>
2024-08-01 19:27:10 +0900

    ⚡️ :: OishiColor 추가

---
+++ app/src/main/java/com/uiel/oishi/design_system/OishiColor.kt
@@ -0,0 +1,18 @@
+package com.uiel.oishi.design_system
+
+import androidx.compose.ui.graphics.Color
+
+data object OishiColor {
+    val purple1 = Color(0xFFB11FF5)
+    val purple2 = Color(0xFFB948EE)
+    val purple3 = Color(0xFFC583F9)
+    val text1 = Color(0xFF1A1E27)
+    val text2 = Color(0xFF505866)
+    val text3 = Color(0xFFD6DADF)
+    val sub1 = Color(0xFF64768C)
+    val sub2 = Color(0xFFB1B8C0)
+    val sub3 = Color(0xFFD6DADF)
+    val gray1 = Color(0xFF2E2E2E)
+    val gray2 = Color(0xFF414347)
+    val white = Color(0xFFFFFFFF)
+}
